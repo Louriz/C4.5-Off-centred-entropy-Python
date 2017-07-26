5b3cd566e2197154bb26274c9eb97d150ad8d213
Louriz <riahilouriz@gmail.com>
2017-07-26 10:13:36 +0200

    add LOO and Entropy well done for categorical features, little changes un datatypes management

--- decision-tree-oce-multiclass-v4.py
+++ decision-tree-oce-multiclass-v4.py
@@ -8,11 +8,137 @@ import sys
 import ast
 import csv
 from collections import Counter
+from collections import OrderedDict
 
+#================== for categorical entropy=================================
+def dataset_to_table(dataset):
+    table={}
+    for k in range(len(dataset.attributes)):
+        table[dataset.attributes[k]]=[]
+        for example in dataset.examples:
+            table[dataset.attributes[k]].append(example[k])
+    return table
+def deldup(li):
+    """ Deletes duplicates from list _li_
+        and return new list with unique values.
+    """
+    return list(OrderedDict.fromkeys(li))
+
+
+def is_mono(t):
+    """ Returns True if all values of _t_ are equal
+        and False otherwise.
+    """
+    for i in t:
+        if i != t[0]:
+            return False
+    return True
+
+
+def get_indexes(table, col, v):
+    """ Returns indexes of values _v_ in column _col_
+        of _table_.
+    """
+    li = []
+    start = 0
+    for row in table[col]:
+        if row == v:
+            index = table[col].index(row, start)
+            li.append(index)
+            start = index + 1
+    return li
+
+
+def get_values(t, col, indexes):
+    """ Returns values of _indexes_ in column _col_
+        of the table _t_.
+    """
+    return [t[col][i] for i in range(len(t[col])) if i in indexes]
+
+
+def del_values(t, ind):
+    """ Creates the new table with values of _ind_.
+    """
+    return {k: [v[i] for i in range(len(v)) if i in ind] for k, v in t.items()}
+
+
+def print_list_tree(tree, tab=''):
+    """ Prints list of nested lists in
+        hierarchical form.
+    """
+    print('%s[' % tab)
+    for node in tree:
+        if isinstance(node, basestring):
+            print('%s  %s' % (tab, node))
+        else:
+            print_list_tree(node, tab + '  ')
+    print('%s]' % tab)
+
+
+def formalize_rules(list_rules):
+    """ Gives an list of rules where
+        facts are separeted by coma.
+        Returns string with rules in
+        convinient form (such as
+        'If' and 'Then' words, etc.).
+    """
+    text = ''
+    for r in list_rules:
+        t = [i for i in r.split(',') if i]
+        text += 'If %s,\n' % t[0]
+        for i in t[1:-1]:
+            text += '   %s,\n' % i
+        text += 'Then: %s.\n' % t[-1]
+    return text
+
+
+def get_subtables(t, col):
+    """ Returns subtables of the table _t_
+        divided by values of the column _col_.
+    """
+    return [del_values(t, get_indexes(t, col, v)) for v in deldup(t[col])]
+
+
+
+def freq(table, col, v):
+    """ Returns counts of variant _v_
+        in column _col_ of table _table_.
+    """
+    return table[col].count(v)
+
+
+def info(table, res_col):
+    """ Calculates the entropy of the table _table_
+        where res_col column = _res_col_.
+    """
+    s = 0 # sum
+    for v in deldup(table[res_col]):
+        p = freq(table, res_col, v) / float(len(table[res_col]))
+        s += p * math.log(p, 2)
+    return -s
+
+
+def infox(table, col, res_col):
+    """ Calculates the entropy of the table _table_
+        after dividing it on the subtables by column _col_.
+    """
+    s = 0 # sum
+    for subt in get_subtables(table, col):
+        s += (float(len(subt[col])) / len(table[col])) * info(subt, res_col)
+    return s
+    
+
+
+def gain(table, x, res_col):
+    """ The criterion for selecting attributes for splitting.
+    """
+    return info(table, res_col) - infox(table, x, res_col)
+
+# ======================== end categorical entropy
 ################Crucial parameters################
 #Off-centred entropy parameter
 theta=0.8
-big_theta=[0.6,0.5,0.3]
+big_theta=[]
 ##################################################
 # data class to hold csv data
 ##################################################
@@ -36,12 +162,24 @@ def read_data(dataset, datafile, datatypes):
 
     #list attributes
     dataset.attributes = dataset.examples.pop(0)
+    #print(dataset.attributes)
 
     
     #create array that indicates whether each attribute is a numerical value or not
-    attr_type = open(datatypes) 
-    orig_file = attr_type.read()
-    dataset.attr_types = orig_file.split(',')
+    #attr_type = open(datatypes) 
+    #orig_file = attr_type.read()
+    #dataset.attr_types = orig_file.split(',')
+    #dataset.attr_types=['true','false','true','false','false','false','false','false','false','false','true','true','true','false','false']
+    #datatypes=['false','false','false','false','true','false']
+    #dataset.attr_types=['true','true','true','true','true','true','true','true','true','true','true','true','true','false']
+    dataset.attr_types=['false','true','false','false','true','false','false','true','false','false','true','false','true','false','false','true','false','true','false','false','false']
+
+
+    #dataset.attr_types=['true','true','true','true','true','true','true','true','true','false'] #  glass
+
+
+
+
 
 ##################################################
 # Preprocess dataset
@@ -50,7 +188,7 @@ def preprocess2(dataset):
     print ("Preprocessing data...")
 
     class_values = [example[dataset.class_index] for example in dataset.examples]
-
+    
     class_mode = Counter(class_values)
     classes=class_mode.keys()
     
@@ -89,7 +227,8 @@ def preprocess2(dataset):
         #convert attributes that are numeric to floats
         for example in dataset.examples:
             for x in range(len(dataset.examples[0])):
-                if dataset.attributes[x] == 'True':
+                if dataset.attr_types[x]=='true':
+                #if dataset.attributes[x] == 'true':
                     example[x] = float(example[x])
 
 ##################################################
@@ -123,7 +262,7 @@ class treeNode():
         # attach tree to the corresponding branch of Tree
     # return tree 
 
-def compute_tree(dataset, parent_node, classifier):
+def compute_tree(dataset, parent_node, classifier,datatypes):
     node = treeNode(True, None, None, None, parent_node, None, None, 0)
     if (parent_node == None):
         node.height = 0
@@ -162,7 +301,7 @@ def compute_tree(dataset, parent_node, classifier):
             for val in attr_value_list:
                 # calculate the gain if we split on this value
                 # if gain is greater than local_max_gain, save this gain and this value
-                local_gain = calc_gain(dataset, dataset_entropy, val, attr_index) # calculate the gain if we split on this value
+                local_gain = calc_gain(dataset, dataset_entropy, val, attr_index,datatypes) # calculate the gain if we split on this value
   
                 if (local_gain >= local_max_gain):
                     local_max_gain = local_gain
@@ -199,8 +338,8 @@ def compute_tree(dataset, parent_node, classifier):
         elif (attr_to_split is not None):
             lower_dataset.examples.append(example)
 
-    node.upper_child = compute_tree(upper_dataset, node, classifier)
-    node.lower_child = compute_tree(lower_dataset, node, classifier)
+    node.upper_child = compute_tree(upper_dataset, node, classifier,datatypes)
+    node.lower_child = compute_tree(lower_dataset, node, classifier,datatypes)
 
     return node
 
@@ -236,6 +375,9 @@ def find_theta(dataset):
     else:
         big_theta=[count[c] / total_examples for c in classes]
     print(theta,big_theta)
+######  
+
+
 ##################################################
 # Calculate the entropy of the current dataset : Entropy(dataset)= - Sigma( pi*log2(pi)) i=1:k  , k is the number of modalities in the class
 ##################################################
@@ -282,27 +424,33 @@ def calc_dataset_entropy(dataset, classifier):  # off centered entropy
 ##################################################
 # Calculate the gain of a particular attribute split
 ##################################################
-def calc_gain(dataset, entropy, val, attr_index):
+def calc_gain(dataset, entropy, val, attr_index,datatypes):
+    table=dataset_to_table(dataset)
     classifier = dataset.attributes[attr_index]
     attr_entropy = 0
-    total_examples = len(dataset.examples);
-    gain_upper_dataset = data(classifier) # instanciate the class data with the parametr classifier
-    gain_lower_dataset = data(classifier)
-    gain_upper_dataset.attributes = dataset.attributes
-    gain_lower_dataset.attributes = dataset.attributes
-    gain_upper_dataset.attr_types = dataset.attr_types
-    gain_lower_dataset.attr_types = dataset.attr_types
-    for example in dataset.examples:
-        if (example[attr_index] >= val):
-            gain_upper_dataset.examples.append(example)
-        elif (example[attr_index] < val):
-            gain_lower_dataset.examples.append(example)
+    if(datatypes[attr_index]=='false'):
+        attr_entropy+=infox(table,dataset.attributes[attr_index],dataset.attributes[len(dataset.attributes)-1])
+        
+        
+    else:
+        total_examples = len(dataset.examples);
+        gain_upper_dataset = data(classifier) # instanciate the class data with the parametr classifier
+        gain_lower_dataset = data(classifier)
+        gain_upper_dataset.attributes = dataset.attributes
+        gain_lower_dataset.attributes = dataset.attributes
+        gain_upper_dataset.attr_types = dataset.attr_types
+        gain_lower_dataset.attr_types = dataset.attr_types
+        for example in dataset.examples:
+            if (example[attr_index] >= val):
+                gain_upper_dataset.examples.append(example)
+            elif (example[attr_index] < val):
+                gain_lower_dataset.examples.append(example)
 
-    if (len(gain_upper_dataset.examples) == 0 or len(gain_lower_dataset.examples) == 0): #Splitting didn't actually split (we tried to split on the max or min of the attribute's range)
-        return -1
+        if (len(gain_upper_dataset.examples) == 0 or len(gain_lower_dataset.examples) == 0): #Splitting didn't actually split (we tried to split on the max or min of the attribute's range)
+            return -1
 
-    attr_entropy += calc_dataset_entropy(gain_upper_dataset, classifier)*len(gain_upper_dataset.examples)/total_examples
-    attr_entropy += calc_dataset_entropy(gain_lower_dataset, classifier)*len(gain_lower_dataset.examples)/total_examples
+        attr_entropy += calc_dataset_entropy(gain_upper_dataset, classifier)*len(gain_upper_dataset.examples)/total_examples
+        attr_entropy += calc_dataset_entropy(gain_lower_dataset, classifier)*len(gain_lower_dataset.examples)/total_examples
 
     return entropy - attr_entropy    # this is the IG ( Information Gain)
 
@@ -470,11 +618,30 @@ def main():
         training_set.rows = []
         test_set = copy.deepcopy(dataset)
         test_set.rows = []
+
         if ("-d" in args):
             datatypes = args[args.index("-d") + 1]
         else:
             datatypes = 'datatypes.csv'
+        datatypes=['false','false','false','false','true','false']  # yellow
+        #datatypes=['true','false','true','false','false','false','false','false','false','false','true','true','true','false','false'] # income
+        #datatypes=['true','true','true','true','true','true','true','true','true','true','true','true','true','false'] # wine
+        #datatypes=['true','true','true','true','true','true','true','true','true','false'] #  glass
+        #datatypes=['false','true','false','false','true','false','false','true','false','false','true','false','true','false','false','true','false','true','false','false','false']
+
+
+
+
+
         read_data(dataset, datafile, datatypes)
+        print("hereeeee", datatypes[4])
+        print(isinstance(dataset.examples[0][4],str))
+        print(dataset.examples[0][4].isdigit())
+        #print(dataset.examples)
+        #print(dataset_to_table(dataset))
+        #table=dataset_to_table(dataset)
+        #print(" here it is ",info(table,"class"))
+
         arg3 = args[2]
         if (arg3 in dataset.attributes):
             classifier = arg3
@@ -482,7 +649,13 @@ def main():
             classifier = dataset.attributes[-1]
 
         dataset.classifier = classifier
-
+        #print("here again",calc_dataset_entropy(dataset,classifier))
+        #table=dataset_to_table(dataset)
+        #attr_entropy=infox(table,dataset.attributes[1],dataset.attributes[len(dataset.attributes)-1])
+        #print("I am here ",attr_entropy)
+        #attr_value_list = [example[0] for example in dataset.examples] # these are the values we can split on, now we must find the best one
+        #attr_value_list = list(set(attr_value_list)) # remove duplicates from list of all attribute values
+        #print(attr_value_list)
         #find index of classifier
         for a in range(len(dataset.attributes)):
             if dataset.attributes[a] == dataset.classifier:
@@ -491,8 +664,42 @@ def main():
                 dataset.class_index = range(len(dataset.attributes))[-1]
                 
         unprocessed = copy.deepcopy(dataset)
+        print(dataset.examples[0])
+        dataset.attr_types=datatypes
         preprocess2(dataset)
         print ("Computing tree...")
+        #============================ LOO: Leave One Out. This part will not be executed unless you pass the paramter "-l". Here you are not obliged to pass the K
+        ##### It is fixed to the number of instances in your dataset.
+        if ("-l" in args):   
+            K=len(dataset.examples)
+
+            prepruning_score=0
+            postpruning_score=0
+            for k in range(K):
+                print ("Doing fold ", k)
+                training_set.examples = [x for i, x in enumerate(dataset.examples) if i % K != k]
+                test_set.examples = [x for i, x in enumerate(dataset.examples) if i % K == k]
+                training_set.attributes=dataset.attributes
+                test_set.attributes=dataset.attributes
+                training_set.class_index=dataset.class_index
+                test_set.class_index=dataset.class_index
+                find_theta(training_set)
+                #print(find_theta(training_set))
+                print ("Number of training records: %d" % len(training_set.examples))
+                #print(training_set.examples)
+                print ("Number of test records: %d" % len(test_set.examples))
+                #print(test_set.examples)
+                root = compute_tree(training_set, None, classifier,datatypes)
+                best_score = validate_tree(root, test_set)
+                prepruning_score+=best_score
+                if ("-p" in args):
+                    post_prune_accuracy = 100*prune_tree(root, root, test_set, best_score)
+                    postpruning_score+=post_prune_accuracy
+
+            print ("Initial (pre-pruning) validation set score for LOO: " + str(100*(prepruning_score/K)) +"%")
+            print ("Post-pruning score on validation set for LOO: " + str(postpruning_score/K) + "%")
+
+        #============================== Cross validation: will not be executed unless you pass the paramter "-k" followed by the number of folds K
         if ("-k" in args):   
             K=int(args[args.index("-k")+1])
 
@@ -512,7 +719,7 @@ def main():
                 #print(training_set.examples)
                 print ("Number of test records: %d" % len(test_set.examples))
                 #print(test_set.examples)
-                root = compute_tree(training_set, None, classifier)
+                root = compute_tree(training_set, None, classifier,datatypes)
                 best_score = validate_tree(root, test_set)
                 prepruning_score+=best_score
                 if ("-p" in args):
@@ -522,24 +729,17 @@ def main():
             print ("Initial (pre-pruning) validation set score for cross validatio: " + str(100*(prepruning_score/K)) +"%")
             print ("Post-pruning score on validation set for cross validation: " + str(postpruning_score/K) + "%")
 
-
-
-
-
-
-
-
-        ##==================================================================================
-        if ("-k" not in args):	
+        #=========================== Train and validation set separated. This part of code will be executed where the paramters "-k" and "-l" not in args============================================================ 
+        if not ("-k" in args or "-l" in args):	
 	        find_theta(dataset)
 	        if  ("-w" in args):
 	            global theta
 	            theta=float(args[args.index("-w")+1])
 	        prepruning_score=0
 	        postpruning_score=0
-	        for i in range(10):
+	        for i in range(1):
 
-	            root = compute_tree(dataset, None, classifier) 
+	            root = compute_tree(dataset, None, classifier,datatypes) 
 	    
 	            if ("-s" in args):
 	                print_disjunctive(root, dataset, "")
@@ -564,35 +764,12 @@ def main():
 	                    print ("Error: You must validate if you want to prune")
 	                else:
 	                    post_prune_accuracy = 100*prune_tree(root, root, validateset, best_score)
+	                    print(post_prune_accuracy)
 	                    postpruning_score+=post_prune_accuracy 
 
 	        print ("Initial (pre-pruning) validation set score: " + str(100*(prepruning_score/10)) +"%")
 	        print ("Post-pruning score on validation set: " + str(postpruning_score/10) + "%")
-        if ("-t" in args):
-            datatest = args[args.index("-t") + 1]
-            testset = data(classifier)
-            read_data(testset, datatest, datatypes)
-            for a in range(len(dataset.attributes)):
-                if testset.attributes[a] == testset.classifier:
-                    testset.class_index = a
-                else:
-                    testset.class_index = range(len(testset.attributes))[-1]
-            print ("Testing model on " + str(datatest))
-            for example in testset.examples:
-                example[testset.class_index] = '0'
-            testset.examples[0][testset.class_index] = '1'
-            testset.examples[1][testset.class_index] = '1'
-            testset.examples[2][testset.class_index] = '?'
-            preprocess2(testset)
-            b = open('results.csv', 'w')
-            a = csv.writer(b)
-            for example in testset.examples:
-                example[testset.class_index] = test_example(example, root, testset.class_index)
-            saveset = testset
-            saveset.examples = [saveset.attributes] + saveset.examples
-            a.writerows(saveset.examples)
-            b.close()
-            print ("Testing complete. Results outputted to results.csv")
+        
             
 if __name__ == "__main__":
 	main()
